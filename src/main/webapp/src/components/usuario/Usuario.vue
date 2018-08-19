a288ec883dc609f7dc7eab094ad0d968f12ab298
Everton Xavier <everton19.xavier@gmail.com>
2018-08-19 19:33:53 -0400

    Configuração inicial do webapp e ajustes iniciais em telas de usuário, home e login

---
+++ src/main/webapp/src/components/usuario/Usuario.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-layout align-center justify-center row fill-height>
+        <v-flex xs12 sm6 lg4>
+            <v-form class="margin-bottom" ref="form" v-model="valid" lazy-validation>
+
+                <v-text-field
+                    v-model="email"
+                    label="E-mail"
+                    required
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="senha"
+                    :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                    :type="showPassword ? 'text' : 'password'"
+                    label="Senha"
+                    required
+                    @click:append="showPassword = !showPassword"
+                ></v-text-field>
+
+                <v-btn
+                    :disabled="!valid"
+                    @click="submit"
+                    color="primary"
+                    class="pull-right"
+                >
+                    Entrar
+                </v-btn>
+
+                <v-btn router-link to="/usuario/add">
+                    <v-icon>person_add</v-icon> Cadastrar-se
+                </v-btn>
+
+            </v-form>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: 'Usuario',
+        data: () => ({
+            valid: true,
+            email: '',
+            senha: '',
+            showPassword: false
+        }),
+        methods: {
+            submit () {
+                if (this.$refs.form.validate()) {
+                    console.log(this);
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .margin-bottom {
+        margin-bottom: 15px;
+    }
+
+    .pull-right {
+        float: right;
+    }
+</style>
---
+++ src/main/webapp/src/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Usuario from './components/usuario/Usuario.vue'
+import UsuarioAdd from './components/usuario/UsuarioAdd.vue'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/usuario',
+      name: 'usuario',
+      component: Usuario
+    },
+    {
+      path: '/usuario/add',
+      name: 'usuario-add',
+      component: UsuarioAdd
+    },
+  ]
+})
