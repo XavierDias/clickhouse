75d8c346910e12a6aef1cb50dda7d0a3f291b0a1
roothery <roothery@gmail.com>
2018-08-19 15:45:54 -0400

    Acrescentado mais dados nos arquivos sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,9 @@
-insert into usuario
+insert into usuario(nome, email, telefone, senha)
 values('Roothery', 'roothery@gmail.com', '67992651815', 'teste123');
 
-insert into usuario
+insert into usuario(nome, email, telefone, senha)
 values('Everton', 'everton.xavier19@gmail.com', '67999131972', 'teste321');
+
+insert into anuncio (tipoanuncio, cidade, estado, rua, bairro, areatotal, areaconstruida, descricao, valor, fk_idusuario)
+values('Casa', 'Campo Grande', 'MS', 'Rua Ataulfo Alves', 'TV Morena', 200000, 150000, 'Casa com bastante espaço',
+1000000, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,24 @@
 create table usuario
 (
-  idusuario int auto_increment,
+  idusuario int primary key auto_increment,
   nome varchar (255) not null,
   email varchar (255) not null,
   telefone varchar (11) not null,
-  senha varchar (15) not null
-  primary key (idusuario)
+  senha varchar (30) not null
+);
+
+create table anuncio
+(
+  idanuncio int primary key auto_increment,
+  tipoanuncio varchar (255) not null,
+  cidade varchar (255) not null,
+  estado char (2) not null,
+  rua varchar (255) not null,
+  bairro varchar (255) not null,
+  areatotal double not null,
+  areaconstruida double not null,
+  descricao varchar (255) not null,
+  valor double not null,
+  fk_idusuario int not null,
+  foreign key (fk_idusuario) references usuario(idusuario) on update cascade
 );
